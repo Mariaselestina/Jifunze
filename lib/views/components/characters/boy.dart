baa14260bc8e5a7dca4ad40c580a88c4848e309a
admin <kalrobbynson@gmail.com>
2020-11-06 18:07:23 +0300

    draggables

--- lib/views/components/characters/boy.dart
+++ lib/views/components/characters/boy.dart
@@ -1,5 +1,4 @@
-
-import 'package:audioplayer/audioplayer.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -9,7 +8,7 @@ class Boy extends StatefulWidget {
 }
 
 class _BoyState extends State<Boy> {
-  AudioPlayer audioPlayer = AudioPlayer();
+  AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
 
   @override
   Widget build(BuildContext context) {
@@ -24,32 +23,50 @@ class _BoyState extends State<Boy> {
               fit: BoxFit.fill,
             ),
           ),
-          
           Positioned(
               bottom: 260,
               left: MediaQuery.of(context).size.width / 2.2,
               child: InkWell(
                   onTap: () {
-                     playLocal();
+                    playLocal();
                     print('no no no');
                   },
                   child: Container(height: 10, width: 24))),
           Positioned(
               bottom: 120,
-              left: MediaQuery.of(context).size.width / 2.4,
-              child: InkWell(
-                  onTap: () {
-                     playLocal();
-                    print('no no no');
-                  },
-                  child: Container(height: 43, width: 80))),
+              left: MediaQuery.of(context).size.width / 2.45,
+              child: DragTarget<Color>(
+                onWillAccept: (value) => value != Colors.black,
+                onAccept: (value) {
+                  //update app state with value
+                },
+                onLeave: (value) {
+                  //Alert the user their value ddint land
+                },
+                builder: (context, candidates, rejects) {
+                  return candidates.length > 0
+                      ? Container(color: Colors.black, height: 60, width: 120)
+                      : Container(color: Colors.grey, height: 60, width: 120);
+                },
+              )),
+          Positioned(
+              top: 10,
+              right: 10,
+              child: Draggable<Color>(
+                  data: Color(0x000000ff),
+                  feedback:
+                      Container(color: Colors.yellow, height: 60, width: 120),
+                  childWhenDragging:
+                      Container(color: Colors.black, height: 60, width: 120),
+                  child:
+                      Container(color: Colors.black, height: 60, width: 120))),
         ],
       ),
     );
   }
 
-   playLocal() async {
-      audioPlayer.play('assets/audio/applauses', isLocal: true);
+  playLocal() async {
+    await audioPlayer.play('assets/audio/crdb_bank.wav', isLocal: true);
     //return(result);
   }
 }
